bb5bc4823debaf2b24fcbb0a5081603e67e407b7
echicken <echicken@bbs.electronicchicken.com>
2018-07-27 13:44:26 -0400

    Use the reusable validators instead of lots of inline comparisons.

--- server/presence/commands.js
+++ server/presence/commands.js
@@ -39,23 +39,7 @@ this.QUERY = function (client, packet) {
       // Update location must be [system_name].[node_number]
       loc.length == 2
       && coa_validate.node_number(loc[1])
-      // Must be an object and not a primitive for the following checks to work
-      && typeof packet.data == 'object'
-      // Must include a valid NodeStatus index (nodedefs.js)
-      && coa_validate.node_status(packet.data.s)
-      // Must include a valid NodeAction index (nodedefs.js)
-      && coa_validate.node_action(packet.data.a)
-      // Must include a valid user alias or empty string
-      && coa_validate.alias(packet.data.u)
-      // May include a custom status string up to 50 characters long
-      && (
-        typeof packet.data.u == 'undefined'
-        || coa_validate.node_status_custom(packet.data.c)
-      )
-      // Update cannot contain extraneous properties
-      && Object.keys(data).every(function (e) {
-        return ['s', 'a', 'u', 'c'].indexOf(e) >= 0;
-      })
+      && coa_validate.presence_node_update(packet.data)
     ) {
       log(LOG_INFO, format(
         'Presence: %s sent an invalid update for %s from %s',
