d61cef50e998e2956f84927ba48539092fd89f3b
echicken <echicken@bbs.electronicchicken.com>
2018-07-30 15:27:26 -0400

    More write/subscribe filtering; comments.

--- server/presence/commands.js
+++ server/presence/commands.js
@@ -26,20 +26,20 @@ this.QUERY = function (client, packet) {
   }
 
   if (packet.oper == 'WRITE') {
-
-    if (loc[1].toUpperCase() != alias.toUpperCase()) {
-      log(alias.toUpperCase());
+    // A system can only write to its own scope (coa_presence[system])
+    if (loc.length < 2 || loc[1].toUpperCase() != alias.toUpperCase()) {
       log(LOG_INFO, format(
         'Presence: %s tried to write to %s from %s',
         admin.authenticated[client.id].alias, loc[1], client.remote_ip_address
       ));
       return true; // Handled
     }
-
+    // If location is coa_presence[system][node]
     if (
-      // Update location must be [system_name].[node_number]
       loc.length == 3
+      // [node] must be valid
       && !coa_validate.node_number(loc[2])
+      // must be a valid node update
       && !coa_validate.presence_node_update(packet.data)
     ) {
       log(LOG_INFO, format(
@@ -48,16 +48,31 @@ this.QUERY = function (client, packet) {
       ));
       return true; // Handled
     }
-
-  } else if (packet.oper == 'SUBSCRIBE' && loc.length > 1) { // location is at least presence[something]
+    // If location is coa_presence[system][node][s,a,u,c]
+    if (loc.length == 4 && (
+      ['s', 'a', 'u', 'c'].indexOf(loc[3]) < 0
+      || (loc[3] == 's' && !coa_validate.node_status(packet.data))
+      || (loc[3] == 'a' && !coa_validate.node_action(packet.data))
+      || (loc[3] == 'u' && !coa_validate.alias(packet.data))
+      || (loc[3] == 'c' && !coa_validate.node_status_custom(packet.data))
+    )) {
+      log(LOG_INFO, format(
+        'Presence: %s sent a invalid update %s for %s from %s',
+        alias, packet.data, packet.location, client.remote_ip_address
+      ));
+      return true; // Handled
+    }
+  // If location is coa_presence[system] or deeper
+  } else if (packet.oper == 'SUBSCRIBE' && loc.length > 1) {
     var ret = false;
-    if (loc.length > 3) {
+    // Can't go any deeper than coa_presence[system][node][attribute]
+    if (loc.length > 4) {
       ret = true;
-    } else if (
-      !coa_validate.alias(loc[1]) || !coa_validate.alias_exists(loc[1])
-    ) {
+    // [system] must be valid and must already exist
+    } else if (!coa_validate.alias_exists(loc[1])) {
       ret = true;
-    } else if (loc.length == 3 && !coa_validate.node_number(loc[2])) {
+    // If specified, [node] must be valid
+    } else if (loc.length >= 3 && !coa_validate.node_number(loc[2])) {
       ret = true;
     }
     if (ret) {
