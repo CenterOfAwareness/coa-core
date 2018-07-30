cde66286bb37ae2e3aa53b96b7b462b016fdb87f
echicken <echicken@bbs.electronicchicken.com>
2018-07-30 15:37:45 -0400

    Rough sketch of presence monitor service.  We'll send log[on/off] announcements from here so that every client doesn't need to subscribe to coa_presence just for those.

--- server/presence/service.js
+++ server/presence/service.js
@@ -1,3 +1,27 @@
 js.time_limit = 0;
 
-// Here's where we'll want to load a COA client object
+require(argv[0] + '../../client/lib/coa.js', 'COA');
+require(argv[0] + '../../client/lib/presence.js', 'COA_Presence');
+// require telegram / announce / broadcast module here
+
+const coa = new COA(host, port, username, password); // Load these parameters from somewhere
+const presence = new COA_Presence(coa);
+// instantiate broadcast announcement thingie here
+
+presence.subscribe(function (update) {
+  switch (update.type) {
+    case 'node_logon':
+      // broadcast a message here
+      break;
+    case 'node_logoff':
+      // broadcast a message here
+      break;
+    default:
+      break;
+  }
+});
+
+while (!js.terminated) {
+  coa.cycle();
+  yield();
+}
