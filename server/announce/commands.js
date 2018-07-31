42976ca02022bd038a8f735cb5c9a7f5512aff84
echicken <echicken@bbs.electronicchicken.com>
2018-07-31 16:35:37 -0400

    Announce query filter; still needs rate limiter.

--- server/announce/commands.js
+++ server/announce/commands.js
@@ -5,7 +5,7 @@ this.QUERY = function (client, packet) {
 
   if (!admin.authenticated[client.id]) {
     log(LOG_DEBUG, format(
-      'Presence: packet from unauthenticated client %s',
+      'Announce: packet from unauthenticated client %s',
       client.remote_ip_address
     ));
     client.close();
@@ -15,6 +15,50 @@ this.QUERY = function (client, packet) {
   const allowed_operations = ['WRITE', 'SUBSCRIBE', 'UNSUBSCRIBE'];
   const loc = packet.location.split('.');
   const alias = admin.authenticated[client.id].alias;
+
+  if (
+    loc[0] != 'coa_announce'
+    || allowed_operations.indexOf(packet.oper) < 0
+    || loc.length != 2
+  ) {
+    log(LOG_INFO, format(
+      'Announce: %s tried %s on %s from %s',
+      alias, packet.oper, packet.location, client.remote_ip_address
+    ));
+    return true; // Handled
+  }
+
+  if (packet.oper == 'WRITE') {
+    // If target is 'global', must be a valid global message
+    if (
+      loc[1] == 'global' && !coa_validate.announce_global_message(packet.data)
+    ) {
+      log(LOG_INFO, format(
+        'Announce: %s sent an invalid global message from %s',
+        admin.authenticated[client.id].alias, client.remote_ip_address
+      ));
+      return true; // Handled
+    // If target is [system], [system] must exist and message must be valid
+    } else if (
+      !coa_validate.alias_exists(loc[1])
+      || !coa_validate.announce_user_message(packet.data)
+    ) {
+      log(LOG_INFO, format(
+        'Announce: %s sent an invalid user message to %s from %s',
+        admin.authenticated[client.id].alias, loc[1], client.remote_ip_address
+      ));
+      return true; // Handled
+    }
+  } else if (packet.oper == 'SUBSCRIBE') {
+    // You can only subscribe to 'global' or your own [system_name]
+    if (loc[1] != 'global' && loc[1] != admin.authenticated[client.id].alias) {
+      log(LOG_INFO, format(
+        'Announce: %s tried to subscribe to %s from %s',
+        admin.authenticated[client.id].alias, loc[1], client.remote_ip_address
+      ));
+      return true;
+    }
+  }
 
   return false; // Request can proceed
 
