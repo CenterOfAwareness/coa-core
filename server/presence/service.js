a52c00ef58347d8894f3342f23f132ed19f818e4
echicken <echicken@bbs.electronicchicken.com>
2018-07-31 22:49:35 -0400

    Omit parameters to COA constructor.

--- server/presence/service.js
+++ server/presence/service.js
@@ -2,11 +2,11 @@ js.time_limit = 0;
 
 require(system.mods_dir + '/coa//client/lib/coa.js', 'COA');
 require(system.mods_dir + '/coa/client/lib/presence.js', 'COA_Presence');
-// require telegram / announce / broadcast module here
+require(system.mods_dir + '/coa/client/lib/announce.js', 'COA_Announce');
 
-const coa = new COA(host, port, username, password); // Load these parameters from somewhere
+const coa = new COA();
 const presence = new COA_Presence(coa);
-// instantiate broadcast announcement thingie here
+const announc = new COA_Announce(coa);
 
 presence.subscribe(function (update) {
   switch (update.type) {
