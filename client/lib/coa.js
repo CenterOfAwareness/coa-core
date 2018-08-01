77c392dbb189a92649ba3b219f0a210a2c901ee9
echicken <echicken@bbs.electronicchicken.com>
2018-08-01 10:54:41 -0400

    Tidy up.

--- client/lib/coa.js
+++ client/lib/coa.js
@@ -1,6 +1,8 @@
 require('json-client.js', 'JSONClient');
 require(system.mods_dir + '/coa/common/settings.js', 'coa_settings');
 
+const SUBSCRIBABLES = ['WRITE', 'PUSH', 'POP', 'SHIFT', 'UNSHIFT', 'DELETE'];
+
 /**
  * An interface to the COA JSON-DB server
  * @constructor COA
@@ -30,25 +32,16 @@ COA.prototype = Object.create(JSONClient.prototype);
 COA.prototype.constructor = COA;
 
 COA.prototype.callback = function (u) {
-  if (
-    // We only care about these types of operations
-    ['WRITE','PUSH','POP','SHIFT','UNSHIFT','DELETE'].indexOf(u.oper) > -1
-    && typeof this.callbacks[u.scope] == 'function'
-  ) {
-    this.callbacks[u.scope](u);
-  }
+  if (SUBSCRIBABLES.indexOf(u.oper) < 0 || !this.callbacks[u.scope]) return;
+  this.callbacks[u.scope](u);
 }
 
 COA.prototype.set_callback = function (db, callback) {
-  if (typeof this.callbacks[db] == 'function') {
-    throw new Error('Callback already registered for ' + db);
-  }
+  if (this.callbacks[db]) throw new Error('Callback already set for ' + db);
   this.callbacks[db] = callback;
 }
 
 COA.prototype.unset_callback = function (db) {
-  if (!this.callbacks[db]) {
-    throw new Error('No callback set for ' + db);
-  }
+  if (!this.callbacks[db]) throw new Error('No callback set for ' + db);
   delete this.callbacks[db];
 }
