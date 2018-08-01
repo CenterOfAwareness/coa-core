cd76e715bbe3721bd1ce04c61db9f85950fa178e
echicken <echicken@bbs.electronicchicken.com>
2018-07-31 23:52:47 -0400

    Typo. Broadcast logon/off messages.

--- server/presence/service.js
+++ server/presence/service.js
@@ -6,15 +6,19 @@ require(system.mods_dir + '/coa/client/lib/announce.js', 'COA_Announce');
 
 const coa = new COA();
 const presence = new COA_Presence(coa);
-const announc = new COA_Announce(coa);
+const announce = new COA_Announce(coa);
 
 presence.subscribe(function (update) {
   switch (update.type) {
     case 'node_logon':
-      // broadcast a message here
+      announce.broadcast(
+        'coa_server', format('%s: %s logged on', update.system, update.user)
+      );
       break;
     case 'node_logoff':
-      // broadcast a message here
+      announce.broadcast(
+        'coa_server', format('%s: %s logged off', update.system, update.user)
+      );
       break;
     default:
       break;
