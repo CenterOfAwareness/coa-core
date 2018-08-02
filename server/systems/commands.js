ac1df07156c721bcb62ee1e4e1aa1e0ef1714dc6
echicken <echicken@bbs.electronicchicken.com>
2018-08-02 16:54:03 -0400

    Allow read of coa_systems, coa_systems[system].  Allow write to coa_systems[system].ping.  Always return true; we don't want to pass the query along or write anything to an actual JSON-DB.

--- server/systems/commands.js
+++ server/systems/commands.js
@@ -16,7 +16,13 @@ this.QUERY = function (client, packet) {
   const loc = packet.location.split('.');
   const alias = admin.authenticated[client.id].alias;
 
-  if (loc[0] != 'coa_systems' || loc.length > 2 || packet.oper != 'READ') {
+  if (
+    loc[0] != 'coa_systems'
+    // You can read coa_systems or coa_systems[system]
+    || (loc.length > 2 && packet.oper == 'READ')
+    // You can write to coa_systems[system].ping but not elsewhere
+    || (loc.length != 3 && packet.oper == 'WRITE')
+  ) {
     log(LOG_INFO, format(
       'Announce: %s tried %s on %s from %s',
       alias, packet.oper, packet.location, client.remote_ip_address
@@ -24,61 +30,85 @@ this.QUERY = function (client, packet) {
     return true; // Handled
   }
 
-  // If they're reading coa_systems
-  if (loc.length == 1) {
+  if (packet.oper == 'READ') {
 
-    const data = [];
-    var usr = new User(0);
-    for (var n = 1; n < system.lastuser; n++) {
-      usr.number = n;
-      if (usr.settings&USER_DELETED) continue;
-      if (!(usr.security.flags1&UFLAG_C)) continue;
-      data.push({
-        name : usr.alias,
-        long_name : usr.name,
-        address : usr.address,
-        joined : usr.birthdate,
-        last_ping : usr.logontime
+    // If they're reading coa_systems
+    if (loc.length == 1) {
+
+      const data = [];
+      var usr = new User(0);
+      for (var n = 1; n < system.lastuser; n++) {
+        usr.number = n;
+        if (usr.settings&USER_DELETED) continue;
+        if (!(usr.security.flags1&UFLAG_C)) continue;
+        data.push({
+          name : usr.alias,
+          long_name : usr.name,
+          address : usr.address,
+          joined : usr.birthdate,
+          last_ping : usr.logontime
+        });
+      }
+      usr = undefined;
+
+      client.sendJSON({
+        scope : 'coa_systems',
+        location : packet.location,
+        func : 'RESPONSE',
+        oper : 'READ',
+        data : data
       });
+
+      return true;
+
+    } else { // loc.length must be 2 if we got here
+
+      var un = system.matchuser(loc[2]);
+      if (un < 1) return false;
+      var usr = new User(un);
+      if (usr.settings&USER_DELETED || !(usr.security.flags1&UFLAG_C)) {
+        return true;
+      }
+      client.sendJSON({
+        scope : 'coa_systems',
+        location : packet.location,
+        func : 'RESPONSE',
+        oper : 'READ',
+        data : {
+          name : usr.alias,
+          long_name : usr.name,
+          address : usr.address,
+          joined : usr.birthdate,
+          last_ping : usr.logontime
+        }
+      });
+
+      return true;
+
     }
-    usr = undefined;
 
-    client.sendJSON({
-      scope : 'coa_systems',
-      location : packet.location,
-      func : 'RESPONSE',
-      oper : 'READ',
-      data : data
-    });
+  } else if (packet.oper == 'WRITE' ) {
 
-    return true;
-
-  } else {
+    if (loc[2] != alias || loc[3] != 'ping') {
+      log(LOG_INFO, format(
+        'Announce: %s tried %s on %s from %s',
+        alias, packet.oper, packet.location, client.remote_ip_address
+      ));
+      return true; // Handled
+    }
 
     var un = system.matchuser(loc[2]);
-    if (un < 1) return false;
+    if (un < 1) return false; // This should be impossible, but meh.
     var usr = new User(un);
     if (usr.settings&USER_DELETED || !(usr.security.flags1&UFLAG_C)) {
-      return false;
+      return true;
     }
-    client.sendJSON({
-      scope : 'coa_systems',
-      location : packet.location,
-      func : 'RESPONSE',
-      oper : 'READ',
-      data : {
-        name : usr.alias,
-        long_name : usr.name,
-        address : usr.address,
-        joined : usr.birthdate,
-        last_ping : usr.logontime
-      }
-    });
-
+    usr.logontime = time();
+    usr = undefined;
     return true;
 
   }
 
-  return false;
+  return true;
 
 }
