5eda636c3ce71a34e34cf8c2386886a2d438fae0
echicken <echicken@bbs.electronicchicken.com>
2018-08-02 22:53:50 -0400

    Use COA_Systems module to ping server on an interval (coa.ini -> [client] -> ping_interval, 300000).

--- client/client.js
+++ client/client.js
@@ -1,13 +1,17 @@
 js.time_limit = 0;
 
 load('nodedefs.js');
+require('event-timer.js', 'Timer');
 require(system.mods_dir + '/coa/client/lib/coa.js', 'COA');
 require(system.mods_dir + '/coa/client/lib/presence.js', 'COA_Presence');
 require(system.mods_dir + '/coa/client/lib/announce.js', 'COA_Announce');
+require(system.mods_dir + '/coa/client/lib/systems.js', 'COA_Systems');
 
+const timer = new Timer();
 const coa = new COA();
 const presence = new COA_Presence(coa);
 const announce = new COA_Announce(coa);
+const systems = new COA_Systems(coa);
 
 function user_online(node) {
   return (node.status == NODE_INUSE || node.status == NODE_QUIET);
@@ -25,8 +29,12 @@ announce.callback = function (update) {
 announce.subscribe('global');
 announce.subscribe('user');
 
+systems.ping();
+timer.addEvent(coa_settings.client.ping_interval, true, systems.ping);
+
 while (!js.terminated) {
   presence.write();
+  timer.cycle();
   coa.cycle();
   yield();
 }
