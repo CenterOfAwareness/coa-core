8d194a3b3c3e0ce55e16854910af66213e2cae0b
echicken <echicken@bbs.electronicchicken.com>
2018-08-03 02:09:23 -0400

    callback is not needed here.

--- client/lib/systems.js
+++ client/lib/systems.js
@@ -4,32 +4,18 @@ require(system.mods_dir + '/coa/common/validate.js', 'coa_validate');
  * An interface to the COA Systems database
  * @constructor
  * @param {COA} coa - An instance of the COA object (client/lib/coa.js)
- * @property {function} callback - A function that handles updates<br>
- * The callback will receive one parameter, either an Object or an Array of
- * Objects with the following properties:<br>
- * { name, long_name, address, joined, last_ping }<br>
- * All values are strings except for last_ping, which is a time_t number
  */
 function COA_Systems(coa) {
-  var callback = function () {};
   Object.defineProperty(this, 'coa', { value : coa });
-  Object.defineProperty(this, 'callback', {
-    get : function () { return callback; },
-    set : function (cb) {
-      if (typeof cb == 'function') {
-        coa.set_callback('coa_systems', callback);
-      } else if (typeof cb != 'undefined') {
-        throw new Error('COA_Systems: invalid callback');
-      }
-      callback = cb;
-    }
-  });
 }
 
 /**
  * Get the full list of systems, or details of one system
  * @param {string} [system=undefined] - The name of the system (optional)
- * @returns {object} The requested data
+ * @returns {object} The requested data as an Object or Array of Objects
+ * with the following properties:<br>
+ * { name, long_name, address, joined, last_ping }<br>
+ * All values are strings except for last_ping, which is a time_t number
  */
 COA_Systems.prototype.get = function (system) {
   var path = 'coa_systems';
