00bb4b0ea21adbb239b8714c13ee528243aff43c
echicken <echicken@bbs.electronicchicken.com>
2018-08-11 00:55:35 -0400

    Beginning of msg/xtrn change scanner service. Too tired to finish this tonight. Should keep last state of groups in memory, should broadcast an update message when something is added, changed, or deleted.

---
+++ server/cnf/service.js
@@ -0,0 +1,19 @@
+js.time_limit = 0;
+
+require('event-timer.js', 'Timer');
+require(system.mods_dir + '/coa/common/settings.js', 'coa_settings');
+require(system.mods_dir + '/coa/common/messages.js', 'coa_lib_messages');
+require(system.mods_dir + '/coa/client/lib/coa.js', 'COA');
+
+const state = { msg : {}, xtrn : {} };
+const timer = new Timer();
+const coa = new COA();
+
+coa_settings.export_message_groups.forEach(function (e) {
+  state.msg[e] = coa_lib_messages.load_message_group(e);
+});
+
+while (!js.terminated) {
+  timer.cycle();
+  yield();
+}
