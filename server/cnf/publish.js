a90056f43e6e9c8dbd4e9c2707478439d813e8c9
echicken <echicken@bbs.electronicchicken.com>
2018-10-28 00:17:21 -0400

    Write 'messages' or 'xtrn' to coa_cnf.update to notify that a cnf section has been updated.

---
+++ server/cnf/publish.js
@@ -0,0 +1,11 @@
+require(system.mods_dir + '/coa/client/lib/coa.js', 'COA');
+
+if (argv.length != 1 || ['messages', 'xtrn'].indexOf(argv[0]) < 0) {
+  writeln('Argument must be "messages" or "xtrn"');
+}
+
+writeln('Connecting to COA ...');
+const coa = new COA();
+writeln('Writing update notification ...');
+coa.write('coa_cnf', 'update', argv[0], 2);
+writeln('Done');
