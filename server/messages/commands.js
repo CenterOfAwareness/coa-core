639e8d33da8991935ef800d93080df452f9763c5
echicken <echicken@bbs.electronicchicken.com>
2018-10-31 16:01:54 -0400

    Rough sketch of sync process (in comments)

--- server/messages/commands.js
+++ server/messages/commands.js
@@ -1,3 +1,23 @@
+load('sbbsdefs.js');
+require(system.mods_dir + '/coa/common/settings.js', 'coa_settings');
+require(system.mods_dir + '/coa/common/messages.js', 'coa_lib_messages');
+
+//  Client connects
+//  Client subscribes to coa_messages.sync[system]
+//  For each managed message group
+//    For each sub
+//      Client writes to coa_messages.sync[system]:
+//        { [group]: { [sub]: [msgid] } } msgid is last non-local message ID
+//      Server sends stream of deleted message IDs
+//      Server sends stream of new messages since [msgid]
+//      Server sends completion message
+//      Client sends sends stream of deleted non-local message IDs since [msgid]
+//        Server records these somehow
+//        We may delegate network message deletion rights to some boards
+//        or we may delete messages if a majority of sysops delete them locally
+//      Client sends stream of all (non-deleted) local messages since [msgid]
+//      Client deletes coa_messages.sync[system]
+
 this.QUERY = function (client, packet) {
 
   return false; // Command may proceed
